23f9827cf57420e6beb799e59c8e704f18606f4c
Shreya Das <dshreya943@gmail.com>
2025-05-04 23:02:49 +0530

    learning1

---
+++ main.js
@@ -0,0 +1,12 @@
+var a=8;
+var b=7;
+var ans=a+b;
+console.log(ans);
+
+///arrays
+var arr=[1,2,3,4,5];
+arr.push(8);
+//console.log(arr);
+for(var i=1;i<arr.length;i++){
+    console.log(arr[2]);
+}
